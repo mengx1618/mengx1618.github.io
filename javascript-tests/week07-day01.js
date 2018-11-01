065ba3ae51ec69d735bbde3efa2e570557573666
Xue Meng <Xue@Xues-MacBook-Pro.local>
2018-11-01 09:19:43 -0700

    week 7

---
+++ javascript-tests/week07-day01.js
@@ -0,0 +1,16 @@
+var prog109 = 'Prog109 on Monday';
+
+var a = 10;
+var b = 15;
+
+console.log(prog109);
+console.log(a * b);
+console.log('a + b = ', a+b);
+
+const num = 20;
+
+if(num > 18){
+    console.log("true");
+}else{
+    console.log("false");
+}
