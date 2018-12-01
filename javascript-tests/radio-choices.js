8c11c24c65ae68f56939f4b4dd8f21f3557e7a63
Xue Meng <Xue@Xues-MacBook-Pro.local>
2018-12-01 12:18:46 -0800

    radio-choices

--- javascript-tests/radio-choices.js
+++ javascript-tests/radio-choices.js
@@ -4,12 +4,33 @@ window.onload = () => {
     myForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const value = document.querySelector('input[name="sorter"]:checked').value;
-        if (value.toLowerCase() === 'language') {
+        if (value.toLowerCase() === 'blue') {
+            setColor("blueBackground");
             showChoice(value);
-        } else {
+        } 
+        
+        else if (value.toLowerCase() === 'green') {
+            setColor("greenBackground");
+            showChoice(value);
+        }
+
+        else if (value.toLowerCase() === 'red') {
+            setColor("redBackground");
+            showChoice(value);
+        }
+
+        else if (value.toLowerCase() === 'yellow') {
+            setColor("yellowBackground");
+            showChoice(value);
+        }
+
+        else if (value.toLowerCase() === 'purple') {
+            setColor("purpleBackground");
             showChoice(value);
         }
     });
+
+
 }
 
 // define function showChoice
@@ -18,4 +39,16 @@ function showChoice(value){
     console.log(value);
     const userDisplay = document.getElementById("user-choice");
     userDisplay.textContent = value;
+}
+
+let currentClass = null;
+
+    const setColor = (className) => {
+        console.log("SET CLASS TO", className);
+        var element = document.getElementById("form-section");
+        if (currentClass) {
+            element.classList.remove(currentClass);        
+        }
+        currentClass = className;
+        element.classList.add(className);
 }
